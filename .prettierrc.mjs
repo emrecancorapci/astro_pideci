18822a3e8fd7db4dd5a3d8a4f59fb635fa7ab140
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-10-18 02:45:57 +0300

    add eslint, prettier, tailwind config files

---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  plugins: ['prettier-plugin-astro'],
+  };
